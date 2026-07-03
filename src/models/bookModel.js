6c057db6e30aee7f041a8a4211e59e5729fe8196
parksiwoo2 <siwoo0312@khu.ac.kr>
2026-07-03 21:24:23 +0900

    feat: 모델 등록

---
+++ src/models/bookModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+module.exports = mongoose.model('Book', bookSchema);

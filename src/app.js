fdfc60b57613851b5a6f7648db7f84485fb2c9d0
jungshii <qkasiddl@khu.ac.kr>
2026-07-08 15:25:52 +0900

    Resolve merge conflicts

--- src/app.js
+++ src/app.js
@@ -4,33 +4,33 @@ const dotenv = require('dotenv');
 const passport = require('passport');
 const morgan = require('morgan');
 
-dotenv.config({ path: path.join(__dirname, "..", ".env") });
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
-const connectDB = require("./config/db");
-const apiRouter = require("./routes/api");
+const connectDB = require('./config/db');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(passport.initialize());
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "main.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'main.html'));
 });
 
-app.get("/book-ranking", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "book-ranking.html"));
+app.get('/book-ranking', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'book-ranking.html'));
 });
 
 app.get('/login', (req, res) => {

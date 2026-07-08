464c193386f18a32f93ae9144ba4e5f9908b1687
jungshii <qkasiddl@khu.ac.kr>
2026-07-08 14:35:46 +0900

    fix: add auth config dependencies

---
+++ src/config/passport.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/userModel');
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, async (jwt_payload, done) => {
+      try {
+        const user = await User.findById(jwt_payload.id);
+
+        if(user) {
+          return done(null, user);
+        }
+        else {
+          return done(null, false);
+        }
+      }
+      catch (err) {
+        return done(err, false);
+      }
+    })
+  )
+}
